2c79fcb5e55f1bfea4a5af17e3f4c5c01f0aa7b2
Michael DiBernardo <mikedebo@gmail.com>
2022-01-30 21:19:33 -0500

    Require token auth by default.

--- meep_server/settings.py
+++ meep_server/settings.py
@@ -132,7 +132,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Rest Framework
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
